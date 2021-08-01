706943e5d29eace4aa8e4b0bb28047dfc0647972
Christian Hettlage <hettlage@saao.ac.za>
2021-08-01 19:43:52 +0200

    Fixed instrument repository

--- saltapi/repository/instrument_repository.py
+++ saltapi/repository/instrument_repository.py
@@ -1,24 +1,31 @@
 from sqlalchemy.engine import Connection
 
+from saltapi.repository.bvit_repository import BvitRepository
+from saltapi.repository.hrs_repository import HrsRepository
+from saltapi.repository.rss_repository import RssRepository
+from saltapi.repository.salticam_repository import SalticamRepository
 from saltapi.service.instrument import BVIT, HRS, RSS, Salticam
 
 
 class InstrumentRepository:
     def __init__(self, connection: Connection) -> None:
-        self.connection = connection
+        self.salticam_repository = SalticamRepository(connection)
+        self.rss_repository = RssRepository(connection)
+        self.hrs_repository = HrsRepository(connection)
+        self.bvit_repository = BvitRepository(connection)
 
     def get_salticam(self, salticam_id: int) -> Salticam:
         """Return a Salticam setup."""
-        return f"Salticam with id {salticam_id}"
+        return self.salticam_repository.get(salticam_id)
 
     def get_rss(self, rss_id: int) -> RSS:
         """Return an RSS setup."""
-        return f"RSS with id {rss_id}"
+        return self.rss_repository.get(rss_id)
 
     def get_hrs(self, hrs_id: int) -> HRS:
         """Return an HRS setup."""
-        return f"HRS with id {hrs_id}"
+        return self.hrs_repository.get(hrs_id)
 
     def get_bvit(self, bvit_id: int) -> BVIT:
         """Return a BVIT setup."""
-        return f"BVIT with id {bvit_id}"
+        return self.bvit_repository.get(bvit_id)
