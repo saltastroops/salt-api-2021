902ad0a1fb2ba9f2b3816d119a2180337bd106fe
Christian Hettlage <hettlage@saao.ac.za>
2021-08-04 14:33:13 +0200

    Blocks router and CORS support added

--- saltapi/main.py
+++ saltapi/main.py
@@ -1,21 +1,29 @@
 from typing import Any
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from saltapi.repository.block_repository import BlockRepository
 from saltapi.repository.instrument_repository import InstrumentRepository
 from saltapi.repository.target_repository import TargetRepository
 from saltapi.repository.unit_of_work import UnitOfWork
 from saltapi.service.block_service import BlockService
+from saltapi.web.api.blocks import router as blocks_router
 from saltapi.web.api.proposals import router as proposals_router
-from saltapi.web.schema.block import Block
+
 
 app = FastAPI()
+origins = ["http://127.0.0.1:4200", "http://localhost:4200"]
 
-app.include_router(proposals_router, prefix="/proposals")
+app.add_middleware(
+    CORSMiddleware, allow_origins=origins, allow_credentials=True, allow_methods=["*"]
+)
+
+app.include_router(blocks_router)
+app.include_router(proposals_router)
 
 
-@app.get("/{block_id}", response_model=Block)
+@app.get("/{block_id}", response_model=Any)
 def home(block_id: int) -> Any:
     with UnitOfWork() as unit_of_work:
         # proposals_dir = Settings().proposals_dir
