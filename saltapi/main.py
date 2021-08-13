60f458b33a3f38447257a4537635f27fb47d81b0
Nhlavutelo Macebele <macebelene@gmail.com>
2021-08-13 13:42:10 +0200

    Authentication and authorisation
    Authored by Nhlavutelo Macebele

--- saltapi/main.py
+++ saltapi/main.py
@@ -1,28 +1,18 @@
-from typing import Any
-
 from fastapi import FastAPI
-
-from saltapi.repository.block_repository import BlockRepository
-from saltapi.repository.instrument_repository import InstrumentRepository
-from saltapi.repository.target_repository import TargetRepository
-from saltapi.repository.unit_of_work import UnitOfWork
-from saltapi.service.block_service import BlockService
+from starlette.middleware.cors import CORSMiddleware
+from saltapi.settings import Settings
 from saltapi.web.api.proposals import router as proposals_router
-from saltapi.web.schema.block import Block
+from saltapi.web.api.authentication import router as authentication_router
 
 app = FastAPI()
+origins = [Settings().frontend_uri]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(proposals_router, prefix="/proposals")
-
-
-@app.get("/{block_id}", response_model=Block)
-def home(block_id: int) -> Any:
-    with UnitOfWork() as unit_of_work:
-        # proposals_dir = Settings().proposals_dir
-        target_repository = TargetRepository(unit_of_work.connection)
-        instrument_repository = InstrumentRepository(unit_of_work.connection)
-        block_repository = BlockRepository(
-            target_repository, instrument_repository, unit_of_work.connection
-        )
-        block_service = BlockService(block_repository)
-        return block_service.get_block(block_id)
+app.include_router(authentication_router, prefix="/authentication")
