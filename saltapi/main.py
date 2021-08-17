6c7107ba26c37fea18ad4f31a8669c91f5e517aa
Mofokeng-C <mofokeng.chk@gmail.com>
2021-08-17 10:09:44 +0200

    backend error logging

--- saltapi/main.py
+++ saltapi/main.py
@@ -1,9 +1,12 @@
+import logging
 import os
 import sys
 sys.path.append(os.path.dirname(os.path.abspath('.')))
 
+
+from typing import Any
+
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 
 from saltapi.logging_config import setup_logging
 from saltapi.repository.block_repository import BlockRepository
@@ -11,19 +14,23 @@ from saltapi.repository.instrument_repository import InstrumentRepository
 from saltapi.repository.target_repository import TargetRepository
 from saltapi.repository.unit_of_work import UnitOfWork
 from saltapi.service.block_service import BlockService
-from saltapi.web.api.blocks import router as blocks_router
 from saltapi.web.api.proposals import router as proposals_router
-
+from saltapi.web.schema.block import Block
 
 app = FastAPI()
 
-setup_logging(app)
+setup_logging()
 
-origins = ["http://127.0.0.1:4200", "http://localhost:4200"]
+app.include_router(proposals_router, prefix="/proposals")
 
-app.add_middleware(
-    CORSMiddleware, allow_origins=origins, allow_credentials=True, allow_methods=["*"]
-)
-
-app.include_router(blocks_router)
-app.include_router(proposals_router)
+@app.get("/{block_id}", response_model=Block)
+def home(block_id: int) -> Any:
+    with UnitOfWork() as unit_of_work:
+        # proposals_dir = Settings().proposals_dir
+        target_repository = TargetRepository(unit_of_work.connection)
+        instrument_repository = InstrumentRepository(unit_of_work.connection)
+        block_repository = BlockRepository(
+            target_repository, instrument_repository, unit_of_work.connection
+        )
+        block_service = BlockService(block_repository)
+        return block_service.get_block(block_id)
