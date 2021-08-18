337d1d26f83fc2757f665abedb5e9b39e340ff32
Christian Hettlage <hettlage@saao.ac.za>
2021-08-18 16:54:36 +0200

    main.py updated

--- saltapi/main.py
+++ saltapi/main.py
@@ -1,36 +1,23 @@
-import logging
-import os
-import sys
-sys.path.append(os.path.dirname(os.path.abspath('.')))
-
-
-from typing import Any
-
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from saltapi.logging_config import setup_logging
-from saltapi.repository.block_repository import BlockRepository
-from saltapi.repository.instrument_repository import InstrumentRepository
-from saltapi.repository.target_repository import TargetRepository
-from saltapi.repository.unit_of_work import UnitOfWork
-from saltapi.service.block_service import BlockService
+from saltapi.settings import Settings
+from saltapi.web.api.authentication import router as authentication_router
+from saltapi.web.api.blocks import router as blocks_router
 from saltapi.web.api.proposals import router as proposals_router
-from saltapi.web.schema.block import Block
 
 app = FastAPI()
 
 setup_logging(app)
 
-app.include_router(proposals_router, prefix="/proposals")
+settings = Settings()
+origins = [settings.frontend_uri]
 
-@app.get("/{block_id}", response_model=Block)
-def home(block_id: int) -> Any:
-    with UnitOfWork() as unit_of_work:
-        # proposals_dir = Settings().proposals_dir
-        target_repository = TargetRepository(unit_of_work.connection)
-        instrument_repository = InstrumentRepository(unit_of_work.connection)
-        block_repository = BlockRepository(
-            target_repository, instrument_repository, unit_of_work.connection
-        )
-        block_service = BlockService(block_repository)
-        return block_service.get_block(block_id)
+app.add_middleware(
+    CORSMiddleware, allow_origins=origins, allow_credentials=True, allow_methods=["*"], allow_headers=["*"]
+)
+
+app.include_router(blocks_router)
+app.include_router(proposals_router)
+app.include_router(authentication_router)
