80170af29e69217c8fd1e470b405180d0c095ee7
Christian Hettlage <hettlage@saao.ac.za>
2021-08-18 13:32:16 +0200

    Added missing API handler

--- saltapi/main.py
+++ saltapi/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from saltapi.settings import Settings
+from saltapi.web.api.authentication import router as authentication_router
 from saltapi.web.api.blocks import router as blocks_router
 from saltapi.web.api.proposals import router as proposals_router
 
@@ -15,3 +16,4 @@ app.add_middleware(
 
 app.include_router(blocks_router)
 app.include_router(proposals_router)
+app.include_router(authentication_router)
