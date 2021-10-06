09db6e6d049174e4bb01cd48b755de5e397429f4
Christian Hettlage <hettlage@saao.ac.za>
2021-10-06 17:39:48 +0200

    Minor bug fixes

--- saltapi/web/schema/block.py
+++ saltapi/web/schema/block.py
@@ -35,7 +35,7 @@ class BlockStatus(BaseModel):
     value: BlockStatusValue = Field(
         ..., title="Block status value", description="Block status value"
     )
-    reason: str = Field(
+    reason: Optional[str] = Field(
         ..., title="Block status reason", description="Block status reason"
     )
 
