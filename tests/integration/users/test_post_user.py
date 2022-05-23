858e57e51d823258466566fd59dd2d9e3719649d
Mofokeng-C <mofokeng.chk@gmail.com>
2022-05-23 09:02:04 +0200

    Post user tests updates

--- tests/integration/users/test_post_user.py
+++ tests/integration/users/test_post_user.py
@@ -1,15 +1,11 @@
 import uuid
 from typing import Any, Dict, Optional
 
+import pytest
 from fastapi.testclient import TestClient
 from starlette import status
 
-from tests.conftest import (
-    authenticate,
-    find_username,
-    misauthenticate,
-    not_authenticated,
-)
+from tests.conftest import authenticate, misauthenticate, not_authenticated
 
 USERS_URL = "/users/"
 
@@ -28,7 +24,7 @@ def _new_user_details(username: Optional[str] = None) -> Dict[str, Any]:
         email=f"{_username}@example.com",
         given_name=_random_string(),
         family_name=_random_string(),
-        institute_id=5,
+        institution_id=5,
     )
 
 
@@ -50,8 +46,17 @@ def test_post_user_should_be_allowed_for_misauthenticated_user(
     assert response.status_code == status.HTTP_201_CREATED
 
 
-def test_post_user_should_be_allowed_for_authenticated_user(client: TestClient) -> None:
-    username = find_username("Investigator", proposal_code="2019-2-SCI-006")
+@pytest.mark.parametrize(
+    "username",
+    [
+        "rajeev",  # Board member
+        "mbackes",  # TAC chair
+        "Nella",  # SALT astronomer
+    ],
+)
+def test_post_user_should_be_allowed_for_authenticated_user(
+    username: str, client: TestClient
+) -> None:
     authenticate(username, client)
 
     response = client.post(USERS_URL, json=_new_user_details())
@@ -61,8 +66,8 @@ def test_post_user_should_be_allowed_for_authenticated_user(client: TestClient) 
 def test_post_user_should_return_400_if_username_exists_already(
     client: TestClient,
 ) -> None:
-    authenticate(find_username("Administrator"), client)
-    existing_username = find_username("Investigator", proposal_code="2019-2-SCI-006")
+    authenticate("hettlage", client)
+    existing_username = "eric"
 
     response = client.post(
         USERS_URL, json=_new_user_details(username=existing_username)
@@ -75,14 +80,27 @@ def test_post_user_should_create_a_new_user(client: TestClient) -> None:
     new_user_details = _new_user_details()
 
     expected_user = new_user_details.copy()
+    # add affiliation institution of the user
+    expected_user["affiliations"] = [
+        {
+            "department": " ",
+            "institution_id": expected_user["institution_id"],
+            "name": "South African Astronomical Observatory",
+            "partner_code": "RSA",
+            "partner_name": "South Africa",
+        }
+    ]
     del expected_user["password"]
-    del expected_user["institute_id"]
+    del expected_user["institution_id"]
+    expected_user["alternative_emails"] = []
     expected_user["roles"] = []
 
-    # check properties other than the password
+    # check properties other than the password and id of the created user
     response = client.post(USERS_URL, json=new_user_details)
+    created_user = response.json()
+    del created_user["id"]
     assert response.status_code == status.HTTP_201_CREATED
-    assert response.json() == expected_user
+    assert created_user == expected_user
 
     # It would be nice to check the password as well, but as the authentication method
     # is replaced with a dummy one for testing, we cannot easily be achieved. The
