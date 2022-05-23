bb601dad5d717207793541671aee7650b873e0c6
Mofokeng-C <mofokeng.chk@gmail.com>
2022-05-23 19:03:19 +0200

    Regression testing updates for Hrs repository

--- tests/repository/test_hrs_repository.py
+++ tests/repository/test_hrs_repository.py
@@ -1,101 +1,96 @@
-from typing import Any, Callable
+from typing import Any, Callable, Dict
 
+import pytest
 from sqlalchemy.engine import Connection
 
 from saltapi.repository.hrs_repository import HrsRepository
 
-TEST_DATA = "repository/hrs_repository.yaml"
-
 
 def test_top_level_values(
-    db_connection: Connection, testdata: Callable[[str], Any]
+    db_connection: Connection, check_instrument: Callable[[Dict[str, Any]], None]
 ) -> None:
-    data = testdata(TEST_DATA)["top_level_values"]
-    hrs_id = data["hrs_id"]
-    expected_hrs = data["hrs"]
+    hrs_id = 639
     hrs_repository = HrsRepository(db_connection)
     hrs = hrs_repository.get(hrs_id)
+    print(hrs)
+    assert "id" in hrs
+    assert "observation_time" in hrs
+    assert "overhead_time" in hrs
+    check_instrument(hrs)
 
-    assert hrs["id"] == hrs_id
-    assert hrs["observation_time"] == expected_hrs["observation_time"]
-    assert hrs["overhead_time"] == expected_hrs["overhead_time"]
 
-
+@pytest.mark.parametrize("hrs_id", [1358, 261, 1615])
 def test_configuration(
-    db_connection: Connection, testdata: Callable[[str], Any]
+    hrs_id: int,
+    db_connection: Connection,
+    check_instrument: Callable[[Dict[str, Any]], None],
 ) -> None:
-    data = testdata(TEST_DATA)["configuration"]
-    for d in data:
-        hrs_id = d["hrs_id"]
-        expected_configuration = d["configuration"]
-        hrs_repository = HrsRepository(db_connection)
-        hrs = hrs_repository.get(hrs_id)
-        configuration = hrs["configuration"]
-
-        assert configuration == expected_configuration
-
-
-def test_mode(db_connection: Connection, testdata: Callable[[str], Any]) -> None:
-    data = testdata(TEST_DATA)["mode"]
-    for d in data:
-        hrs_id = d["hrs_id"]
-        expected_mode = d["mode"]
-        hrs_repository = HrsRepository(db_connection)
-        hrs = hrs_repository.get(hrs_id)
-        mode = hrs["configuration"]["mode"]
-
-        assert mode == expected_mode
-
-
-def test_target_location(
-    db_connection: Connection, testdata: Callable[[str], Any]
-) -> None:
-    data = testdata(TEST_DATA)["target_location"]
-    for d in data:
-        hrs_id = d["hrs_id"]
-        expected_location = d["location"]
-        hrs_repository = HrsRepository(db_connection)
-        hrs = hrs_repository.get(hrs_id)
-        location = hrs["configuration"]["target_location"]
-
-        assert location == expected_location
-
-
-def test_iodine_cell_position(
-    db_connection: Connection, testdata: Callable[[str], Any]
-) -> None:
-    data = testdata(TEST_DATA)["iodine_cell_position"]
-    for d in data:
-        hrs_id = d["hrs_id"]
-        expected_position = d["position"]
-        hrs_repository = HrsRepository(db_connection)
-        hrs = hrs_repository.get(hrs_id)
-        position = hrs["configuration"]["iodine_cell_position"]
-
-        assert position == expected_position
-
-
-def test_detectors(db_connection: Connection, testdata: Callable[[str], Any]) -> None:
-    data = testdata(TEST_DATA)["detectors"]
-    for d in data:
-        hrs_id = d["hrs_id"]
-        expected_blue_detector = d["blue_detector"]
-        expected_red_detector = d["red_detector"]
-        hrs_repository = HrsRepository(db_connection)
-        hrs = hrs_repository.get(hrs_id)
-        blue_detector = hrs["blue_detector"]
-        red_detector = hrs["red_detector"]
-
-        assert blue_detector == expected_blue_detector
-        assert red_detector == expected_red_detector
-
-
-def test_procedure(db_connection: Connection, testdata: Callable[[str], Any]) -> None:
-    data = testdata(TEST_DATA)["procedure"]
-    hrs_id = data["hrs_id"]
-    expected_procedure = data["procedure"]
     hrs_repository = HrsRepository(db_connection)
     hrs = hrs_repository.get(hrs_id)
-    procedure = hrs["procedure"]
+    assert "configuration" in hrs
+    check_instrument(hrs)
 
-    assert procedure == expected_procedure
+
+@pytest.mark.parametrize("hrs_id", [2319, 2298, 2320, 2327])
+def test_mode(
+    hrs_id: int,
+    db_connection: Connection,
+    check_instrument: Callable[[Dict[str, Any]], None],
+) -> None:
+    hrs_repository = HrsRepository(db_connection)
+    hrs = hrs_repository.get(hrs_id)
+
+    assert "configuration" in hrs
+    assert "mode" in hrs["configuration"]
+    check_instrument(hrs)
+
+
+@pytest.mark.parametrize("hrs_id", [244, 2328, 1829])
+def test_target_location(
+    hrs_id: int,
+    db_connection: Connection,
+    check_instrument: Callable[[Dict[str, Any]], None],
+) -> None:
+    hrs_repository = HrsRepository(db_connection)
+    hrs = hrs_repository.get(hrs_id)
+
+    assert "target_location" in hrs["configuration"]
+    check_instrument(hrs)
+
+
+@pytest.mark.parametrize("hrs_id", [2297, 2320, 1615])
+def test_iodine_cell_position(
+    hrs_id: int,
+    db_connection: Connection,
+    check_instrument: Callable[[Dict[str, Any]], None],
+) -> None:
+    hrs_repository = HrsRepository(db_connection)
+    hrs = hrs_repository.get(hrs_id)
+
+    assert "iodine_cell_position" in hrs["configuration"]
+    check_instrument(hrs)
+
+
+@pytest.mark.parametrize("hrs_id", [2316, 80])
+def test_detectors(
+    hrs_id: int,
+    db_connection: Connection,
+    check_instrument: Callable[[Dict[str, Any]], None],
+) -> None:
+    hrs_repository = HrsRepository(db_connection)
+    hrs = hrs_repository.get(hrs_id)
+
+    assert "blue_detector" in hrs
+    assert "red_detector" in hrs
+    check_instrument(hrs)
+
+
+def test_procedure(
+    db_connection: Connection, check_instrument: Callable[[Dict[str, Any]], None]
+) -> None:
+    hrs_id = 385
+    hrs_repository = HrsRepository(db_connection)
+    hrs = hrs_repository.get(hrs_id)
+
+    assert "procedure" in hrs
+    check_instrument(hrs)
