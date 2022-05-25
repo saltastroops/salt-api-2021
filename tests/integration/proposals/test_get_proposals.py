70340718ef5166271d35d7cf52d27d816a93225e
Mofokeng-C <mofokeng.chk@gmail.com>
2022-05-25 11:38:59 +0200

    Proposals integration tests updates

--- tests/integration/proposals/test_get_proposals.py
+++ tests/integration/proposals/test_get_proposals.py
@@ -67,13 +67,6 @@ def test_should_return_422_for_invalid_semesters(
     "username,from_semester,to_semester,proposal_count,proposal_codes",
     [
         [
-            find_username("TAC Chair", partner_code="RSA"),
-            "2020-1",
-            "2020-1",
-            32,
-            "many",
-        ],
-        [
             find_username("TAC Chair", partner_code="RU"),
             "2021-1",
             "2021-1",
