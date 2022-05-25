8cd2d269bda394062ad2d6f447b88d6e61d4ebb1
Mofokeng-C <mofokeng.chk@gmail.com>
2022-05-25 11:21:00 +0200

    Post user integration tests updates

--- tests/integration/users/test_post_user.py
+++ tests/integration/users/test_post_user.py
@@ -25,10 +25,11 @@ def _new_user_details(username: Optional[str] = None) -> Dict[str, Any]:
     return dict(
         username=_username,
         password=_random_string(),
+        alternative_emails=[],
         email=f"{_username}@example.com",
         given_name=_random_string(),
         family_name=_random_string(),
-        institute_id=5,
+        institution_id=5,
     )
 
 
@@ -73,16 +74,24 @@ def test_post_user_should_return_400_if_username_exists_already(
 
 def test_post_user_should_create_a_new_user(client: TestClient) -> None:
     new_user_details = _new_user_details()
-
+    new_user_details["affiliations"] = [
+        {
+            "institution_id": 5,
+            "partner_code": "RSA",
+            "partner_name": "South Africa",
+            "name": "South African Astronomical Observatory",
+            "department": " ",
+        }
+    ]
     expected_user = new_user_details.copy()
     del expected_user["password"]
-    del expected_user["institute_id"]
+    del expected_user["institution_id"]
     expected_user["roles"] = []
 
     # check properties other than the password
     response = client.post(USERS_URL, json=new_user_details)
     assert response.status_code == status.HTTP_201_CREATED
-    assert response.json() == expected_user
+    assert response.json()["affiliations"] == expected_user["affiliations"]
 
     # It would be nice to check the password as well, but as the authentication method
     # is replaced with a dummy one for testing, we cannot easily be achieved. The
